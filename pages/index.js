959e589ee48c82083401de7ae57d5325cd0dd12a
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2026-06-20 16:57:41 +0330

    Show customers on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
+import connectDB from "../utils/connectDB";
+import Customer from "../models/Customer";
+import HomePage from "../components/template/HomePage";
 
 function Index({ customers }) {
-  return <h1>crm project</h1>;
+  return <HomePage customers={customers} />;
 }
 
 export default Index;
+
+export async function getServerSideProps() {
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers)),
+      },
+    };
+  } catch (err) {
+    return {
+      notFound: true,
+    };
+  }
+}
 
